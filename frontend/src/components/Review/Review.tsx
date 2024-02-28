83a2b9c6042e526794cd02cf43059d280a1f0454
Isa Scollo <iscollo4@gmail.com>
2024-02-28 13:07:57 +1100

    finalised review page

--- frontend/src/components/Review/Review.tsx
+++ frontend/src/components/Review/Review.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import styles from "./Review.module.css";
+import Bees from "../Bees/Bees";
 
 // Assuming this is the shape of your review data
 interface ReviewType {
@@ -20,6 +21,14 @@ function Review() {
   // Initialize the review state with a null value, and explicitly type it as ReviewType | null
   const [review, setReview] = useState<ReviewType | null>(null);
 
+  const navigate = useNavigate();
+
+  // Function to handle button click
+  const navigateHome = () => {
+    navigate('/'); // Assuming '/' is your home route
+  };
+
+
   useEffect(() => {
     // Fetch the review details based on `title`
     fetch(`/api/reviews/${title}`)
@@ -30,18 +39,30 @@ function Review() {
 
   // Conditionally render the review content or a loading message
   return (
-    <div>
-      {review ? (
-        <div>
-          <h1>{review.title}</h1>
-          <div className={styles.Content}>
-            <div className={styles.Text}>{review.reviewText}</div>
-            {review.image && <img src={review.image} alt={review.title} />}
+    <div className={styles.Container}>
+      <button onClick={navigateHome}>Back</button>
+      <div className={styles.ReviewContainer}>
+        {review ? (
+          <div className={styles.ContentContainer}>
+            <div className={styles.Header}>
+              <h1>{review.title}</h1>
+              <p>{review.year}</p>
+            </div>
+            <div className={styles.Content}>
+              <div className={styles.LeftContainer}>
+                <div className={styles.Text}>{review.reviewText}</div>
+                <Bees
+                  isaRating={review.isaRating}
+                  seanRating={review.isaRating}
+                />
+              </div>
+                {review.image && <img src={review.image} alt={review.title} />}
+            </div>
           </div>
-        </div>
-      ) : (
-        <div>Loading review...</div> // Display a loading message or spinner when data is being fetched
-      )}
+        ) : (
+          <div>Loading review...</div> // Display a loading message or spinner when data is being fetched
+        )}
+      </div>
     </div>
   );
 }
